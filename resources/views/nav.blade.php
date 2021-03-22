12596e4f3885b20877b5f5285e7de4ce7cf9d7e0
toto <toto0909@icloud.com>
2021-03-22 21:51:07 +0900

    軽微な修正

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
 
-    <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
+    <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>popsns</a>
   
     <ul class="navbar-nav ml-auto">
   
