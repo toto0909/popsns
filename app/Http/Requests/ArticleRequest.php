956329388937ff1b026d1ad30ab2ffd50d27bcbe
toto <toto0909@icloud.com>
2021-03-22 21:41:30 +0900

    記事投稿model, controller作成

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //==========ここから追加==========
+            'title' => 'required|max:50',
+            'body' => 'required|max:500',
+            //==========ここまで追加==========
+        ];
+    }
+
+    //==========ここから追加==========
+    public function attributes()
+    {
+        return [
+            'title' => 'タイトル',
+            'body' => '本文',
+        ];
+    }
+    //==========ここまで追加==========
+}
