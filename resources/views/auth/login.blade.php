5cd75d7b28dac9b4946c222127bb074a3a0f9ddd
toto <toto0909@icloud.com>
2021-03-23 20:21:17 +0900

    メールパスワード再設定 view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,6 +31,10 @@
                 <input type="checkbox" name="remember" id="remember" value="on">次回から自動でログインする
                 {{--ここまで--}}
 
+                <div class="text-left">
+                    <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                </div>
+
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
               </form>
